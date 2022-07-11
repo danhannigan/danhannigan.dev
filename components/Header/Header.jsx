aae5edce2c61b00b27591661ddb9639fa7edc0b6
Dan Hannigan <hi@dan-h.dev>
2022-07-11 16:44:54 -0600

    Fixin a little zindex bug

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 function Header() {
   return (
-    <header className="fixed block h-10 min-w-full bg-white/70 py-2 backdrop-blur-lg dark:bg-slate-800/70">
+    <header className="fixed z-50 block h-10 min-w-full bg-white/70 py-2 backdrop-blur-lg dark:bg-slate-800/70">
       <div className="container mx-auto flex max-w-prose justify-between px-4 xl:px-0">
         <div className="hidden lg:block">
           <Link href="/">
