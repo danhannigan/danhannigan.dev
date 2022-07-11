f68a23cb2ad25625015ae981a88ebcc3cd8ac241
Dan Hannigan <hi@dan-h.dev>
2022-07-11 15:51:07 -0600

    testing Umami code

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <script
+          async
+          defer
+          data-website-id="4124e679-f902-4288-ac2a-c07861eaa94d"
+          src="https://luminous-sunburst-e0c391.netlify.app/umami.js"
+        ></script>
+      </Head>
       <body className="bg-white dark:bg-slate-900">
         <Main />
         <NextScript />
