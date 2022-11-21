9a7ffdea7fa82ed4d3aa1a27f0ec7298a7373cad
Dan Hannigan <hi@dan-h.dev>
2022-11-20 22:34:15 -0700

    feat: add rel me links

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -6,24 +6,24 @@ import ArrowIcon from "../ArrowLink/Arrow.svg";
 function Footer() {
   return (
     <footer className="md:text-1xl border-t border-t-background-accent-dark p-4 font-accent text-lg font-bold uppercase tracking-widest text-background-accent-neutral md:py-6 md:pl-14">
-      <div className="">
+      <div>
         <div>
           <ul className="flex flex-wrap gap-4 md:gap-12">
             <li className="flex items-center gap-2 md:gap-3">
               <ArrowIcon className="h-4  md:h-4" />
               <Link href="/">
-                <a className="">Home</a>
+                <a>Home</a>
               </Link>
             </li>
             <li className="flex items-center gap-2 md:gap-3">
               <ArrowIcon className="h-4  md:h-4" />
-              <a href="https://github.com/danhannigan" className=" ">
+              <a href="https://github.com/danhannigan" rel="me">
                 Github
               </a>
             </li>
             <li className="flex items-center gap-2 md:gap-3">
               <ArrowIcon className="h-4  md:h-4" />
-              <a href="https://twitter.com/danhannigan" className="">
+              <a href="https://twitter.com/danhannigan" rel="me">
                 Twitter
               </a>
             </li>
@@ -35,13 +35,13 @@ function Footer() {
             </li>
             <li className="flex items-center gap-2 md:gap-3">
               <ArrowIcon className="h-4  md:h-4" />
-              <a href="https://www.linkedin.com/in/danhannigan/" className="">
+              <a href="https://www.linkedin.com/in/danhannigan/" rel="me">
                 Linkedin
               </a>
             </li>
             <li className="flex items-center gap-2 md:gap-3">
               <ArrowIcon className="h-4  md:h-4" />
-              <a href="https://www.polywork.com/danhannigan" className="">
+              <a href="https://www.polywork.com/danhannigan" rel="me">
                 Polywork
               </a>
             </li>
