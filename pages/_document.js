a03fce807de789339059b0e48dfe64330c98a536
Dan Hannigan <hi@dan-h.dev>
2022-11-20 22:40:00 -0700

    feat: add webmentions

--- pages/_document.js
+++ pages/_document.js
@@ -32,6 +32,14 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;700&family=Josefin+Sans:wght@300;400;700&family=Lato&display=swap"
           rel="stylesheet"
         ></link>
+        <link
+          rel="webmention"
+          href="https://webmention.io/www.danhannigan.dev/webmention"
+        />
+        <link
+          rel="pingback"
+          href="https://webmention.io/www.danhannigan.dev/xmlrpc"
+        />
       </Head>
       <body className="bg-background-default text-text-default">
         <Main />
