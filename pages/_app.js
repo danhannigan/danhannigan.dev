f4da88cd0c8a8e859b2b0d75fa1051f3e0270d3a
Dan Hannigan <hi@dan-h.dev>
2023-01-08 16:41:38 -0700

    adding plausible

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import "../styles/globals.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { Analytics } from "@vercel/analytics/react";
+import PlausibleProvider from "next-plausible";
 
 import * as ga from "../lib/ga";
 import Transition from "../components/Transition";
@@ -23,35 +24,37 @@ function App({ Component, pageProps, router }) {
   }, [router.events]);
 
   return (
-    <div className="flex h-screen flex-col justify-between">
-      <DefaultSeo
-        titleTemplate="%s | Dan Hannigan"
-        canonical={url}
-        openGraph={{
-          type: "website",
-          locale: "en_US",
-          url,
-          description:
-            "Personal website for Dan Hannigan, front end developer, designer, and community leader in Denver, Colorado.",
-          site_name: "Dan Hannigan | danhannigan.dev",
-        }}
-        twitter={{
-          handle: "@danhannigan",
-          site: "@danhannigan",
-          cardType: "summary",
-        }}
-      />
-      <Header />
-      <AnimatePresence
-        mode="wait"
-        initial={false}
-        onExitComplete={() => window.scrollTo(0, 0)}
-      >
-        <Component {...pageProps} canonical={url} key={url} />
-        <Analytics />
-      </AnimatePresence>
-      <Footer />
-    </div>
+    <PlausibleProvider domain="danhannigan.dev">
+      <div className="flex h-screen flex-col justify-between">
+        <DefaultSeo
+          titleTemplate="%s | Dan Hannigan"
+          canonical={url}
+          openGraph={{
+            type: "website",
+            locale: "en_US",
+            url,
+            description:
+              "Personal website for Dan Hannigan, front end developer, designer, and community leader in Denver, Colorado.",
+            site_name: "Dan Hannigan | danhannigan.dev",
+          }}
+          twitter={{
+            handle: "@danhannigan",
+            site: "@danhannigan",
+            cardType: "summary",
+          }}
+        />
+        <Header />
+        <AnimatePresence
+          mode="wait"
+          initial={false}
+          onExitComplete={() => window.scrollTo(0, 0)}
+        >
+          <Component {...pageProps} canonical={url} key={url} />
+          <Analytics />
+        </AnimatePresence>
+        <Footer />
+      </div>
+    </PlausibleProvider>
   );
 }
 
