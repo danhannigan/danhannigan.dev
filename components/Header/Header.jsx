df7b9cf498e3dec795df7edf44102a8e54bae5ca
Dan Hannigan <hi@dan-h.dev>
2023-01-22 10:16:16 -0700

    fix: rewind link display

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -15,47 +15,48 @@ function Header() {
         </a>
       </Link>
       <div className="ml-auto flex">
-        {router.pathname === "/" ? (
-          <Link href="/2022" scroll={false}>
-            <a className="mr-8 flex text-text-muted transition duration-300 ease-in-out hover:text-white ">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="mr-1 h-6 w-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M21 16.811c0 .864-.933 1.405-1.683.977l-7.108-4.062a1.125 1.125 0 010-1.953l7.108-4.062A1.125 1.125 0 0121 8.688v8.123zM11.25 16.811c0 .864-.933 1.405-1.683.977l-7.108-4.062a1.125 1.125 0 010-1.953L9.567 7.71a1.125 1.125 0 011.683.977v8.123z"
-                />
-              </svg>
-              Past
-            </a>
-          </Link>
-        ) : (
-          <Link href="/" scroll={false}>
-            <a className="mr-8 flex text-text-muted transition duration-300 ease-in-out hover:text-white ">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="mr-1 h-6 w-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M3 8.688c0-.864.933-1.405 1.683-.977l7.108 4.062a1.125 1.125 0 010 1.953l-7.108 4.062A1.125 1.125 0 013 16.81V8.688zM12.75 8.688c0-.864.933-1.405 1.683-.977l7.108 4.062a1.125 1.125 0 010 1.953l-7.108 4.062a1.125 1.125 0 01-1.683-.977V8.688z"
-                />
-              </svg>
-              Present
-            </a>
-          </Link>
-        )}
+        {router.pathname !== "/post/[slug]" &&
+          (router.pathname === "/" ? (
+            <Link href="/2022" scroll={false}>
+              <a className="mr-8 flex text-text-muted transition duration-300 ease-in-out hover:text-white ">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="mr-1 h-6 w-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M21 16.811c0 .864-.933 1.405-1.683.977l-7.108-4.062a1.125 1.125 0 010-1.953l7.108-4.062A1.125 1.125 0 0121 8.688v8.123zM11.25 16.811c0 .864-.933 1.405-1.683.977l-7.108-4.062a1.125 1.125 0 010-1.953L9.567 7.71a1.125 1.125 0 011.683.977v8.123z"
+                  />
+                </svg>
+                Past
+              </a>
+            </Link>
+          ) : (
+            <Link href="/" scroll={false}>
+              <a className="mr-8 flex text-text-muted transition duration-300 ease-in-out hover:text-white ">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="mr-1 h-6 w-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3 8.688c0-.864.933-1.405 1.683-.977l7.108 4.062a1.125 1.125 0 010 1.953l-7.108 4.062A1.125 1.125 0 013 16.81V8.688zM12.75 8.688c0-.864.933-1.405 1.683-.977l7.108 4.062a1.125 1.125 0 010 1.953l-7.108 4.062a1.125 1.125 0 01-1.683-.977V8.688z"
+                  />
+                </svg>
+                Present
+              </a>
+            </Link>
+          ))}
         <Link href="/rss/feed.xml" scroll={false}>
           <a className="ml-auto text-accent" target="_blank">
             <svg
